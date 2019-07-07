cbda5aca3e3c174726219ced8ba84740cfed69b2
Jesus Valentin Mendoza Torres <jmendoto@everis.com>
2019-07-07 00:15:34 -0500

    remove html template error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import express, { json } from 'express';
-import morgan from 'morgan';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import { join } from 'path';
-import 'dotenv/config';
+import express, { json } from 'express'
+import morgan from 'morgan'
+import { connect } from 'mongoose'
+import cors from 'cors'
+import 'dotenv/config'
 
 import indexRoute from './routes/index'
 import postsRoute from './routes/post'
@@ -33,12 +32,12 @@ app.use('/api/movies/', movieRoute)
 app.use('/api/movierental/', movieRentalRoute)
 
 app.use((req, res, next) => {
-    res.sendFile(join(__dirname + '/error/404.html'));
+    res.status(404).json({ message: "Not Found" });
 })
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.sendFile(join(__dirname + '/error/503.html'));
+    res.status(503).json({ message: "503" });
 })
 
 export default app;
